b4c6d55639309c2fcb3483172ddd078c344947d6
stella <zzj070417@qq.com>
2026-04-29 23:33:47 +0800

    chore!:从sp_vision_25迁移文件。

--- tools/crc.hpp
+++ tools/crc.hpp
@@ -1,20 +1,22 @@
-#ifndef TGU_ROBOCORE_2027_CRC_HPP
-#define TGU_ROBOCORE_2027_CRC_HPP
+#ifndef TOOLS__CRC_HPP
+#define TOOLS__CRC_HPP
 
 #include <cstdint>
 
-namespace tools {
-    // len不包括crc8
-    uint8_t get_crc8(const uint8_t *data, uint16_t len);
+namespace tools
+{
+// len不包括crc8
+uint8_t get_crc8(const uint8_t * data, uint16_t len);
 
-    // len包括crc8
-    bool check_crc8(const uint8_t *data, uint16_t len);
+// len包括crc8
+bool check_crc8(const uint8_t * data, uint16_t len);
 
-    // len不包括crc16
-    uint16_t get_crc16(const uint8_t *data, uint32_t len);
+// len不包括crc16
+uint16_t get_crc16(const uint8_t * data, uint32_t len);
 
-    // len包括crc16
-    bool check_crc16(const uint8_t *data, uint32_t len);
-} // namespace tools
+// len包括crc16
+bool check_crc16(const uint8_t * data, uint32_t len);
 
-#endif //TGU_ROBOCORE_2027_CRC_HPP
+}  // namespace tools
+
+#endif  // TOOLS__CRC_HPP
--- tools/logger.hpp
+++ tools/logger.hpp
@@ -1,93 +1,12 @@
-//
-// Created by tgu on 2026/4/14.
-//
+#ifndef TOOLS__LOGGER_HPP
+#define TOOLS__LOGGER_HPP
 
-#ifndef TGU_ROBOCORE_2027_LOGGER_HPP
-#define TGU_ROBOCORE_2027_LOGGER_HPP
+#include <spdlog/spdlog.h>
 
-#pragma once
+namespace tools
+{
+std::shared_ptr<spdlog::logger> logger();
 
-#include <fstream>
-#include <mutex>
-#include <string>
-#include <format>
+}  // namespace tools
 
-namespace tools {
-    enum class LogLevel {
-        Debug = 0,
-        Info,
-        Warn,
-        Error,
-        Off
-    };
-
-    struct LoggerConfig {
-        LogLevel level = LogLevel::Debug;
-        bool enable_console = true;
-        bool enable_file = false;
-        std::string file_path = "log.txt";
-    };
-
-    class Logger {
-    public:
-        static Logger &instance();
-
-        void init(const LoggerConfig &config);
-
-        void log(LogLevel level,
-                 const std::string &module,
-                 const std::string &msg,
-                 const char *file,
-                 int line);
-
-    private:
-        Logger() = default;
-
-        ~Logger();
-
-        std::string format(LogLevel level,
-                           const std::string &module,
-                           const std::string &msg,
-                           const char *file,
-                           int line) const;
-
-    private:
-        LogLevel level_ = LogLevel::Debug;
-        bool console_ = true;
-        bool file_ = false;
-
-        std::ofstream ofs_;
-        std::mutex mutex_;
-    };
-} // namespace tools
-
-
-#define LOG_INFO(module, fmt, ...) \
-::tools::Logger::instance().log( \
-::tools::LogLevel::Info, module, \
-std::format(fmt __VA_OPT__(,) __VA_ARGS__), \
-__FILE__, __LINE__)
-
-#ifdef NDEBUG
-#define LOG_DEBUG(module, fmt, ...) ((void)0)
-#else
-#define LOG_DEBUG(module, fmt, ...) \
-::tools::Logger::instance().log( \
-::tools::LogLevel::Debug, module, \
-std::format(fmt __VA_OPT__(,) __VA_ARGS__), \
-__FILE__, __LINE__)
-#endif
-
-#define LOG_WARN(module, fmt, ...) \
-::tools::Logger::instance().log( \
-::tools::LogLevel::Warn, module, \
-std::format(fmt __VA_OPT__(,) __VA_ARGS__), \
-__FILE__, __LINE__)
-
-#define LOG_ERROR(module, fmt, ...) \
-::tools::Logger::instance().log( \
-::tools::LogLevel::Error, module, \
-std::format(fmt __VA_OPT__(,) __VA_ARGS__), \
-__FILE__, __LINE__)
-
-#endif //TGU_ROBOCORE_2027_LOGGER_HPP
+#endif  // TOOLS__LOGGER_HPP
